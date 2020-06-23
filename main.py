79258fa0e6f11cdbceeeb6c80234a02103000c88
Pedro Custodio <pedro.custodio@aluno.ufes.br>
2020-06-23 12:27:33 -0300

    Teste do 'inline'

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import telegram
 from telegram.ext import Updater
 from telegram.ext import CommandHandler
 from telegram.ext import MessageHandler, Filters
+from telegram import InlineQueryResultArticle, InputTextMessageContent
+from telegram.ext import InlineQueryHandler
 
 import logging
 import time
@@ -15,7 +17,6 @@ TOKEN = sys.argv[1]
 updater = Updater(token=TOKEN, use_context=True)
 
 dispatcher = updater.dispatcher
-
 
 def start(update, context):
     context.bot.send_message(chat_id=update.effective_chat.id, text="I'm a bot, please talk to me!")
@@ -37,6 +38,23 @@ def caps(update, context):
 
 caps_handler = CommandHandler('caps', caps)
 dispatcher.add_handler(caps_handler)
+
+def inline_caps(update, context):
+    query = update.inline_query.query
+    if not query:
+        return
+    results = list()
+    results.append(
+        InlineQueryResultArticle(
+            id=query.upper(),
+            title='Caps',
+            input_message_content=InputTextMessageContent(query.upper())
+        )
+    )
+    context.bot.answer_inline_query(update.inline_query.id, results)
+
+inline_caps_handler = InlineQueryHandler(inline_caps)
+dispatcher.add_handler(inline_caps_handler)
 
 def unknown(update, context):
     context.bot.send_message(chat_id=update.effective_chat.id, text="Sorry, I didn't understand that command.")
