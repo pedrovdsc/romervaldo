82370edaf27277fdb81278c8457be04be2c13dbf
Pedro Custodio <pedro.custodio@aluno.ufes.br>
2020-06-24 23:36:11 -0300

    Descobri como acessar as mensagens respondidas

--- main.py
+++ main.py
@@ -26,14 +26,39 @@ dispatcher.add_handler(start_handler)
 
 updater.start_polling()
 
-def echo(update, context):
-    context.bot.send_message(chat_id=update.effective_chat.id, text=update.message.text)
+# def echo(update, context):
+#     context.bot.send_message(chat_id=update.effective_chat.id, text=update.message.text)
 
-echo_handler = MessageHandler(Filters.text & (~Filters.command), echo)
-dispatcher.add_handler(echo_handler)
+# def echo(update, context):
+#     """Echo the user message."""
+#     pprint('########################')
+#     pprint(update.message.chat.username)
+#     pprint(update.message.chat.id)
+#     pprint('-----------------------')
+#     pprint(update.message.reply_to_message)
+#     pprint('########################')
+#     update.message.reply_text(update.message.text)
+
+# echo_handler = MessageHandler(Filters.text & (~Filters.command), echo)
+# dispatcher.add_handler(echo_handler)
 
 def caps(update, context):
-    text_caps = ' '.join(context.args).upper()
+    print(context.args)
+    print(len(context.args))
+    #text_caps = ' '.join(context.args).upper()
+    replied_message = update.message.reply_to_message
+    text_caps = replied_message.text.upper()
+    # pprint('########################')
+    # pprint(update.message.chat.username)
+    # pprint(update.message.chat.id)
+    # pprint('-----------------------')
+    # pprint(update.message.reply_markup)
+    # pprint('-----------------------')
+    # pprint(dir(update.message.reply_markdown_v2))
+    # pprint('-----------------------')
+    # pprint(dir(update.message.reply_markdown))
+    # pprint('########################')
+    #update.message.reply_text(update.message.text)
     context.bot.send_message(chat_id=update.effective_chat.id, text=text_caps)
 
 caps_handler = CommandHandler('caps', caps)
@@ -41,6 +66,9 @@ dispatcher.add_handler(caps_handler)
 
 def tofrench(update, context):
     text_french = 'Omelette au fromage!'
+    if context.args == []:
+        context.bot.send_message(chat_id=update.effective_chat.id, text="Tu es bête?")
+        return
     context.bot.send_message(chat_id=update.effective_chat.id, text=text_french)
 
 tofrench_handler = CommandHandler('tofrench', tofrench)
@@ -70,7 +98,3 @@ unknown_handler = MessageHandler(Filters.command, unknown)
 dispatcher.add_handler(unknown_handler)
 
 print ('Bot ligado')
-
-
-
-
