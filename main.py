daaf1b914e120d23358d5226f4d7fb112d4ab782
Pedro Custodio <pedro.custodio@aluno.ufes.br>
2020-08-01 19:00:33 -0300

    romer agora e capaz de traduzir

--- main.py
+++ main.py
@@ -4,19 +4,19 @@ from telegram.ext import CommandHandler
 from telegram.ext import MessageHandler, Filters
 from telegram import InlineQueryResultArticle, InputTextMessageContent
 from telegram.ext import InlineQueryHandler
+from googletrans import Translator
 
 import logging
 import time
 import sys
 from pprint import pprint
 
-logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
-
 TOKEN = sys.argv[1]
 
 updater = Updater(token=TOKEN, use_context=True)
-
 dispatcher = updater.dispatcher
+
+translator = Translator()
 
 def start(update, context):
     context.bot.send_message(chat_id=update.effective_chat.id, text="I'm a bot, please talk to me!")
@@ -69,27 +69,51 @@ dispatcher.add_handler(caps_handler)
 def tofrench(update, context):
     replied_message = update.message.reply_to_message
     
-    text_french = 'Omelette au fromage!'
     if replied_message.text == '':
         context.bot.send_message(chat_id=update.effective_chat.id, text="Tu es bête?")
         return
-    update.message.reply_text(text=text_french)
-    #context.bot.send_message(chat_id=update.effective_chat.id, text=text_french)
+    translated = translator.translate(replied_message.text, dest='fr')
+    update.message.reply_text(text=translated.text)
 
-tofrench_handler = CommandHandler('tofrench', tofrench)
+tofrench_handler = CommandHandler('tofr', tofrench)
 dispatcher.add_handler(tofrench_handler)
 
 def toitalian(update, context):
     replied_message = update.message.reply_to_message
-    text_italian = 'Gli uomini scrivono nello zucchero'
+    
     if replied_message.text == '':
         context.bot.send_message(chat_id=update.effective_chat.id, text="Sei stupido?")
         return
-    update.message.reply_text(text=text_italian)
-    #context.bot.send_message(chat_id=update.effective_chat.id, text=text_italian)
+    translated = translator.translate(replied_message.text, dest='it')
+    update.message.reply_text(text=translated.text)
 
-toitalian_handler = CommandHandler('toitalian', toitalian)
+toitalian_handler = CommandHandler('toit', toitalian)
 dispatcher.add_handler(toitalian_handler)
+
+def tojapanese(update, context):
+    replied_message = update.message.reply_to_message
+    
+    if replied_message.text == '':
+        context.bot.send_message(chat_id=update.effective_chat.id, text="馬鹿")
+        return
+    translated = translator.translate(replied_message.text, dest='ja')
+    update.message.reply_text(text=translated.text)
+
+tojapanese_handler = CommandHandler('toja', tojapanese)
+dispatcher.add_handler(tojapanese_handler)
+
+def togerman(update, context):
+    replied_message = update.message.reply_to_message
+    
+    if replied_message.text == '':
+        context.bot.send_message(chat_id=update.effective_chat.id, text="Dumm!")
+        return
+    translated = translator.translate(replied_message.text, dest='de')
+    update.message.reply_text(text=translated.text)
+
+togerman_handler = CommandHandler('toge', togerman)
+dispatcher.add_handler(togerman_handler)
+
 
 ## END OF TO LANGUAGE FUNCTIONS ##
 
